abc8ca3a998800a6f0d5062380d68fe3edcf670d
mtti <mtti@users.noreply.github.com>
2019-09-21 18:00:31 +0300

    Fix an adorable typo

--- src/expectSingle.ts
+++ src/expectSingle.ts
@@ -2,7 +2,7 @@
  * Return the first item in an array if it only has one item, otherwise throw
  * an error.
  *
- * @param source An item or an awway of items.
+ * @param source An item or an array of items.
  */
 export function expectSingle<T>(source: T|T[]): T {
   if (Array.isArray(source)) {
